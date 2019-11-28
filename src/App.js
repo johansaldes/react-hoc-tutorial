fc40619770b3fb2d502f6fd4ac8a0c248bc2eec6
Johan Öhman Saldes <johan.saldes@m-fvfzl1bul413.localdomain>
2019-11-28 12:46:31 +0100

    added second part

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ const withSecret = Component => class extends React.Component {
   state = {
      secret: 'Very53cr37Token'
   }
+  setSecret = e => this.setState({
+    secret: e.target.value
+  })
   render() {
      return (
-        <Component secret={this.state.secret} />
+        <Component secret={this.state.secret} setSecret={this.setSecret} />
      )
   }
 }
 
 
-const App = ({ secret }) => (
- <div>{secret}</div>
+const App = ({ secret, setSecret }) => (
+ <div>
+   {secret}
+   <input type="text" onChange={setSecret} />
+ </div>
 );
 
 export default withSecret(App);
